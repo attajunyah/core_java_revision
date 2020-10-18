30f56e6cb4dbcba2234c8bcd2915938707adb1bc
Frimpong Atta Junior Osei <61680263+attajunyah@users.noreply.github.com>
2020-10-19 05:23:33 +0800

    upload countvowels

---
+++ CountVowels.java
@@ -0,0 +1,23 @@
+package com.corejava;
+
+import java.util.Scanner;
+
+public class CountVowels
+{
+   public static void main(String[] args)
+   {
+      Scanner in = new Scanner(System.in);
+      System.out.print("Enter a word: ");
+      String word = in.next();
+      int vowels = 0;
+      for (int i = 0; i < word.length(); i++)
+      {
+         String ch = word.substring(i, i + 1);
+         if ("aeiou".contains(ch))
+         {
+        	 vowels++;
+         }
+      }
+      System.out.println("Vowels: " + vowels);
+   }
+}
